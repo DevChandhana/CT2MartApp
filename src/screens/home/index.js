7756228a25c2f9a981ea9b2b0168801a264bdd99
Chandhana <chandhanareddy1997@gmail.com>
2022-10-28 12:12:03 +0530

    home header

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {login, authSelector} from '../../redux/features/authSlice';
+// components
+import Header from './components/header';
+// styles
+import {styles} from './styles';
 
 const Home = () => {
   const user = useSelector(authSelector);
   console.log(user);
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <Header />
       <Text>Home page</Text>
     </SafeAreaView>
   );
